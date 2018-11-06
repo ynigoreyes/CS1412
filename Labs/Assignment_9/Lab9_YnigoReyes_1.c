fce2647e4bbc3cd102bebe0f5bfc6ab9d3511145
ynigoreyes <ynigoreyes@gmail.com>
2018-11-05 23:11:02 -0600

    adding lab 9

--- Labs/Assignment_9/Lab9_YnigoReyes_1.c
+++ Labs/Assignment_9/Lab9_YnigoReyes_1.c
@@ -13,25 +13,35 @@
 //
 //    Function main begins program execution
 
+void bubbleSort(char arr[][MAX_LENGTH], int n) {
+}
+
 int main() {
-  char ** messages = (char **)malloc(sizeof(char *)*NUM_MESSAGES);
-  for(int i = 0; i < NUM_MESSAGES; i++){
-    messages[i] = (char *)malloc(sizeof(char) * MAX_LENGTH);
-  }
+  printf("How many strings would you like to encrypt?\n");
+  int maxAmt;
+  scanf("%d", &maxAmt);
+  getchar();
+
+  char messages[maxAmt][MAX_LENGTH];
   char choice;
 
   char message[100];
   char *newMessage;
 
+  // Even and Odd
   char firstPart[50];
   char secondPart[50];
+  newMessage = "";
+  memset(firstPart, 0, sizeof(firstPart));
+  memset(secondPart, 0, sizeof(secondPart));
 
   // Get user input
-  int i = 0;
+  int stringCount = 0; // How many strings we have saved
   do {
     printf("Enter a string: ");
     gets(message);
     int j = 0, k = 0;
+
     // Go through and sort out the elements by index
     for(int i = 0; i < strlen(message); i++) {
       // even
@@ -46,15 +56,17 @@ int main() {
       }
     }
 
+    firstPart[k+1] = '\0';
+    secondPart[j+1] = '\0';
+
     k = 0;
     j = 0;
 
     newMessage = strcat(firstPart, secondPart);
-    printf("%s\n", newMessage);
-    // TODO: Save into Array for printing in order
-    messages[i] = newMessage;
-    printf("%s %s\n", newMessage, messages[i]);
-    i++;
+    printf("Your encrypted message is: %s\n", newMessage);
+    strcpy(messages[stringCount], newMessage);
+
+    stringCount++;
     // reset
     newMessage = "";
     memset(firstPart, 0, sizeof(firstPart));
@@ -63,12 +75,53 @@ int main() {
     printf("Would you like to continue (Y/N) ? ");
     scanf("%c", &choice);
     getchar();
-    
-    for(int pos = 0; pos < i; pos++) {
-      printf("%s\n", messages[pos]);
-    }
   } while(choice == 'Y' || choice == 'y');
-  
+
+  // Go through the saved strings and decode in place
+  for(int pos = 0; pos < stringCount; pos++){
+    char *oddChars = (char *)malloc(sizeof(char) * strlen(messages[pos]));
+    char *evenChars = (char *)malloc(sizeof(char) * strlen(messages[pos]));
+
+    // Even chars
+    for(int sop = 0; sop < (strlen(messages[pos]) / 2); sop++) {
+      evenChars[sop] = messages[pos][sop];
+    }
+
+    // Odd Chars
+    int temp = 0;
+    for(int sop = (strlen(messages[pos]) / 2); sop < strlen(messages[pos]); sop++) {
+      oddChars[temp] = messages[pos][sop];
+      temp++;
+    }
+
+    char decrypted[MAX_LENGTH];
+    int temp_2 = 0;
+    for(int i = 0; i < strlen(oddChars); i++) {
+      decrypted[temp_2] = oddChars[i];
+      temp_2++;
+      decrypted[temp_2] = evenChars[i];
+      temp_2++;
+    }
+    decrypted[temp_2 + 1] = '\0';
+    strcpy(messages[pos], decrypted);
+  }
+
+  // Sort the decrypted messages using bubble sort
+  char temp[MAX_LENGTH];
+
+  // Sorting strings using bubble sort
+  for (int j=0; j<maxAmt-1; j++) {
+    for (int i=j+1; i<maxAmt; i++) {
+      if (strcmp(messages[j], messages[i]) > 0) {
+        strcpy(temp, messages[j]);
+        strcpy(messages[j], messages[i]);
+        strcpy(messages[i], temp);
+      }
+    }
+  }
+
+  for(int i = 0; i < maxAmt; i++)
+    printf("%s\n", messages[i]);
 
   return 0;
 }
